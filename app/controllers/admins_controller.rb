57b79b57564f6a6162554b4dad355329c9570c4b
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-15 15:50:54 -0500

    Feat: accepting a pet does not change accepting on a different app

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,16 +1,18 @@
 class AdminsController < ApplicationController
   def show
     @applicant = Applicant.find(params[:id])
+    @applications = @applicant.pet_applicants
   end
 
   def update
     @applicant = Applicant.find(params[:id])
     @pet = Pet.find(params[:pet_id])
     if params[:decision] == 'accept'
-      @application = PetApplicant.find_and_accept(params[:id], params[:pet_id]).first
       @pet.update!(adoptable: false, status: true)
+      @application = PetApplicant.find_and_accept(params[:id], params[:pet_id]).first
       redirect_to "/admin/applications/#{@applicant.id}"
     elsif params[:decision] == 'reject'
+      @decision = false
       @application = PetApplicant.find_and_reject(params[:id], params[:pet_id]).first
       @pet.update!(adoptable: true, status: false)
       redirect_to "/admin/applications/#{@applicant.id}"
--- spec/features/admin/show_spec.rb
+++ spec/features/admin/show_spec.rb
@@ -2,23 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'admin show page' do
   before(:each) do
-    @aurora = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @cherry_creek = Shelter.create(name: 'Cherry Creek shelter', city: 'Denver, CO', foster_program: true, rank: 1)
     @denver = Shelter.create(name: 'Denver shelter', city: 'Denver, CO', foster_program: true, rank: 5)
     @jax = @cherry_creek.pets.create(name: "Jax", age: 1, breed: 'Golden Retriever', adoptable: true, status: 'Pending')
     @boss = @cherry_creek.pets.create(name: "Boss", age: 2, breed: 'German Shepard', adoptable: true, status: 'Pending')
     @luke = @denver.pets.create(name: "Luke", age: 1, breed: 'Huskie', adoptable: true, status: 'Pending')
     @greg = Applicant.create(name: 'Greg Flaherty', street_address: '123 MyStreet St.', city: "Dallas", state: "TX", zipcode: '12345', description: "Love dogs", status: "Pending")
-    PetApplicant.create(pet_id: @jax.id, applicant_id: @greg.id, approved: false)
-    PetApplicant.create(pet_id: @boss.id, applicant_id: @greg.id, approved: false)
-    PetApplicant.create(pet_id: @luke.id, applicant_id: @greg.id, approved: false)
+    PetApplicant.create(pet_id: @jax.id, applicant_id: @greg.id, approved: false, status: "Pending")
+    PetApplicant.create(pet_id: @boss.id, applicant_id: @greg.id, approved: false, status: "Pending")
+    PetApplicant.create(pet_id: @luke.id, applicant_id: @greg.id, approved: false, status: "Pending")
   end
 
   describe 'approve a specific pet' do
     it 'For every pet, I see a button to approve the application for that specific pet' do
       visit "/admin/applications/#{@greg.id}"
       expect(current_path).to eq("/admin/applications/#{@greg.id}")
-
+      save_and_open_page
       expect(page).to have_button("Accept #{@jax.name}")
       expect(page).to have_button("Accept #{@boss.name}")
       expect(page).to have_button("Accept #{@luke.name}")
@@ -30,6 +29,7 @@ RSpec.describe 'admin show page' do
 
       click_on "Accept #{@jax.name}"
       expect(current_path).to eq("/admin/applications/#{@greg.id}")
+      save_and_open_page
       expect(page).to have_content("#{@jax.name} has been approved")
     end
   end
@@ -51,7 +51,23 @@ RSpec.describe 'admin show page' do
       click_on "Reject #{@luke.name}"
       expect(current_path).to eq("/admin/applications/#{@greg.id}")
       expect(page).to have_content("#{@luke.name} has been rejected")
-      save_and_open_page
+    end
+
+    it 'accepting a pet on app doesnt accept it on another' do
+      @katie = Applicant.create(name: 'Katie Sanger', street_address: '123 MyStreet St.', city: "Dallas", state: "TX", zipcode: '12345', description: "Love dogs", status: "Pending")
+      PetApplicant.create(pet_id: @luke.id, applicant_id: @katie.id, approved: false)
+
+      visit "/admin/applications/#{@greg.id}"
+      expect(current_path).to eq("/admin/applications/#{@greg.id}")
+      click_on "Accept #{@luke.name}"
+      expect(current_path).to eq("/admin/applications/#{@greg.id}")
+      expect(page).to have_content("#{@luke.name} has been approved")
+
+      visit "/admin/applications/#{@katie.id}"
+      expect(page).to have_button("Accept #{@luke.name}")
+      click_on "Accept #{@luke.name}"
+      expect(current_path).to eq("/admin/applications/#{@katie.id}")
+      expect(page).to have_content("#{@luke.name} has been approved")
     end
   end
 end
