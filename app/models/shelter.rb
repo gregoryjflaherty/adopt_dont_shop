46f86aaa5352af2e19f5168cd1a5ce7061ced0a9
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-16 07:02:23 -0500

    Feat: shelter show page shows number of adoptable pets

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -40,6 +40,10 @@ class Shelter < ApplicationRecord
     pets.where(adoptable: true)
   end
 
+  def adoptable_pet_count
+    pets.where(adoptable: true).count
+  end
+
   def alphabetical_pets
     adoptable_pets.order(name: :asc)
   end
